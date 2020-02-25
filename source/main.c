545adde4428e42e647d0b4c6a24e92ea08f08aee
jortega2 <alexortega1932@hotmail.com>
2020-02-25 12:56:46 -0800

    updated main

--- source/main.c
+++ source/main.c
@@ -11,15 +11,15 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdio.h>
-#include "../header/adc.h"
+#include "adc.h"
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
 #endif
 
 int main(void) {
 	DDRB = 0xFF; PORTB = 0x00;
-	int x, y;
-	unsigned char swi, up, down, left, right;
+	int x, y, xy;
+	unsigned char swi, up, down, left, right = 0;
 	ADC_Init();
 	
     while (1) {
@@ -27,13 +27,23 @@ int main(void) {
 		
 		y = ADC_Read(1);/* Read the status on Y-OUT pin using channel 0 */
 
-		ADC_Value = ADC_Read(2);/* Read the status on SWITCH pin using channel 0 */
-		if(y < 600)
-			swi = 0;	
-		else
-			swi = 1;
+		xy = ADC_Read(2);/* Read the status on SWITCH pin using channel 0 */
 		
+		if (x-y == 0){
+			up = 1;
+			down = left = 0;
+		} else if (x-y >= 700){
+			down = 1;
+			up = left = 0;
+		} else if (x-y <=-700){
+			left = 1;
+			down = up = 0;
+		} 
+		if(y < 600)
+			swi = 1;	
+		else
+			swi = 0;
+		PORTB = swi | up << 1 | down << 2 | left << 3;	
 	}
-    }
     return 1;
 }
