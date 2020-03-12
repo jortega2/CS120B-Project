e1cf1fe253866133aba67f4778432bf43bf69a1b
jortega2 <alexortega1932@hotmail.com>
2020-03-12 11:33:01 -0700

    final build

--- source/main.c
+++ source/main.c
@@ -25,7 +25,7 @@ unsigned long _avr_timer_M = 1; // Start count from here, down to 0. Default 1 m
 unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1ms ticks 
 unsigned char score;
 char buffer[5];
-short cnt, symbol;
+short cnt, symbol, reaction;
 
 int x, y;
 
@@ -66,20 +66,17 @@ void TimerSet(unsigned long M){
 	_avr_timer_cntcurr = _avr_timer_M;
 }
 
-enum screens {s_init, off, wait, countdown,  play, gameOver, gameOverWait} state;
+enum screens {s_init, off, wait, countdown, play, gameOver, gameOverWait} state;
 
 void TickFct_screen(){
 		switch (state){
 			case s_init:
 				state = off;
-				cnt = 0;
-				score = 0;
-				lcd_setXY(0x50, 0x05);
-				N5110_Data("off");
+				cnt = score = reaction = 0;
+				lcd_setXY(0x40, 0x80);
+				N5110_Data("Press button  to start");
 				break;
 			case off:
-				PORTD = 0x01;	
-				cnt++;
 				if ((~PINA & 0x08) == 0x08){
 					//PORTD = 0x02;
 					symbol = cnt%4;
@@ -90,72 +87,33 @@ void TickFct_screen(){
 				}	
 				break;
 			case wait:
-				PORTD = 0x0F;
-				cnt++;
-				N5110_clear();
 				if (cnt > 50){
-                                        if (symbol == 0){
-                                                //N5110_clear();
-                                                lcd_setXY(0x40, 0x80);
-                                                //N5110_Data("up wait");
-						N5110_image(up);
-                                        } else if (symbol == 1){
-                                                //N5110_clear();
-                                                lcd_setXY(0x40, 0x80);
-                                                //N5110_Data("left wait");
-						N5110_image(left);
-                                        } else if (symbol == 2){
-                                                //N5110_clear();
-                                                lcd_setXY(0x40, 0x80);
-                                                //N5110_Data("right wait");
-						N5110_image(right);
-                                        } else if (symbol == 3){
-                                                //N5110_clear();
-                                                lcd_setXY(0x40, 0x80);
-                                                //N5110_Data("down wait");
-						N5110_image(down);
-                                        } else {
-						lcd_setXY(0x40, 0x80);
-						N5110_Data("defaulted...");
-					}
-					cnt = 0;
 					state = play;
 				} else {
 					state = wait;
 				}
 				break;
 			case countdown:
-				PORTD = 0x03;
-				if (cnt == 0){	
-					N5110_clear();
-					N5110_image(three);
-				} else if (cnt == 1000){
-					N5110_clear();
-					N5110_image(two);
-				} else if (cnt == 2000){
-					N5110_clear();
-					N5110_image(one);
-				}
 				if (cnt < 3000){
 					state = countdown;
 				} else {
 					cnt = 0;
 					state = wait;
 				}
-				cnt++;
+				//cnt++;
 				break;
 			case play:
-				PORTD = 0x04;
-				//cnt++;
-				lcd_setXY(0x40, 0x80);
-				//N5110_Data(cnt);
-				if (cnt >= 600){
-					//PORTD = 0xFF;
+				if ((~PINA & 0x08) == 0x08){
+					cnt = 0;
+					state = countdown;
+				} else if (cnt >= 600){
+				 	//PORTD = 0xFF;
 					cnt = 0;
 					state = gameOver;
 				} else if ((y > 900) && (x > 400 && x < 600)){
 					if (symbol == 0){
                                                 symbol = cnt%4;
+						reaction += cnt;
                                                 cnt = 0;
                                                 score++;
 						state = wait;
@@ -166,6 +124,7 @@ void TickFct_screen(){
 				} else if ((y < 100) && (x > 400 && x < 600)){
 					if (symbol == 3){
                                                 symbol = cnt%4;
+						reaction += cnt;
                                                 cnt = 0;
                                                 score++;
 						state = wait;
@@ -176,6 +135,7 @@ void TickFct_screen(){
 				} else if ((x < 100) && (y > 400 && y < 600)) {
 					if (symbol == 2){
                                                 symbol = cnt%4;
+						reaction += cnt;
                                                 cnt = 0;
                                                 score++;
 						state = wait;
@@ -186,6 +146,7 @@ void TickFct_screen(){
 				} else if ((x > 900) && (y > 400 && y < 600)){
 					if (symbol == 1){
                                                 symbol = cnt%4;
+						reaction += cnt;
                                                 cnt = 0;
                                                 score++;
 						state = wait;
@@ -199,12 +160,6 @@ void TickFct_screen(){
 				}
 				break;
 			case gameOver:
-				N5110_clear();
-				lcd_setXY(0x01, 0x05);
-				N5110_Data("gameOver      Score:");
-				sprintf(buffer, "%d", score);
-				N5110_Data(buffer);
-				N5110_Data("       Press button 1 to play again");
 				state = gameOverWait;
 				break;
 			case gameOverWait:
@@ -217,7 +172,89 @@ void TickFct_screen(){
 				break;
 			default:
 				break;
-		}
+		}//transitions
+		switch (state){
+			case s_init:
+				cnt = score = reaction = 0;
+                                lcd_setXY(0x40, 0x80);
+                                N5110_Data("off");
+				break;
+			case off:
+				//PORTD = 0x01;
+                                cnt++;
+				if (cnt >= 1000){
+					cnt  = 0;
+				}
+				break;
+			case wait:
+				//PORTD = 0x0F;
+                                cnt++;
+                                N5110_clear();
+				if (cnt > 50){
+                                        if (symbol == 0){
+                                                //N5110_clear();
+                                                lcd_setXY(0x40, 0x80);
+                                                //N5110_Data("up wait");
+                                                N5110_image(up);
+                                        } else if (symbol == 1){
+                                                //N5110_clear();
+                                                lcd_setXY(0x40, 0x80);
+                                                //N5110_Data("left wait");
+                                                N5110_image(left);
+                                        } else if (symbol == 2){
+                                                //N5110_clear();
+                                                lcd_setXY(0x40, 0x80);
+                                                //N5110_Data("right wait");
+                                                N5110_image(right);
+                                        } else if (symbol == 3){
+                                                //N5110_clear();
+                                                lcd_setXY(0x40, 0x80);
+                                                //N5110_Data("down wait");
+                                                N5110_image(down);
+                                        } else {
+                                                lcd_setXY(0x40, 0x80);
+                                                N5110_Data("defaulted...");
+                                        }
+				}
+				break;
+			case countdown:
+				lcd_setXY(0x40, 0x80);
+				if (cnt == 0){
+                                        N5110_clear();
+                                        N5110_image(three);
+                                } else if (cnt == 1000){
+                                        N5110_clear();
+                                        N5110_image(two);
+                                } else if (cnt == 2000){
+                                        N5110_clear();
+                                        N5110_image(one);
+                                }
+				cnt++;
+				break;
+			case play:
+				//PORTD = 0x04;
+                                lcd_setXY(0x40, 0x80);
+				break;
+			case gameOver:
+				N5110_clear();
+                                lcd_setXY(0x01, 0x05);
+                                N5110_Data("GameOver      Score:");
+                                sprintf(buffer, "%d", score);
+                                N5110_Data(buffer);
+				if (score != 0){
+					reaction = (reaction / score);
+				}
+				sprintf(buffer, "%d", reaction);
+				N5110_Data("       Avg Reaction  Time:");
+				N5110_Data(buffer);
+                                N5110_Data("      Press button  to play again");
+				break;
+			case gameOverWait:
+				//PORTD = 0x08;
+				break;
+			default:
+				break;
+	}
 }
 
 enum joystick_states {j_init, j_main} js_state;
